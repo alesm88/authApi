941131d80bf0dd9edbe5c82e3113887aaa80b3d3
Alejandro <alesm91@hotmail.com>
2025-07-22 11:15:57 +0200

    Ajouter des validations pour un utilisateur

---
+++ validations/userValidation.js
@@ -0,0 +1,36 @@
+import Joi from 'joi';
+
+export const createUserSchema = Joi.object({
+  name: Joi.string()
+    .min(2)
+    .max(50)
+    .required()
+    .trim()
+    .messages({
+      'string.empty': `Le nom est requis.`,
+      'string.min': `Le nom doit contenir au moins 2 caractères.`,
+    }),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required()
+    .trim()
+    .messages({
+      'string.email': `Le format de l'email est invalide.`,
+      'string.empty': `L'email est requis.`,
+    }),
+  password: Joi.string()
+    .min(6)
+    .required()
+    .trim()
+    .messages({
+      'string.min': `Le mot de passe doit contenir au moins 6 caractères.`,
+      'string.empty': `Le mot de passe est requis.`,
+    }),
+    isVerified: Joi.boolean()
+    .default(false)
+    .valid(false)
+    .messages({
+        'boolean.valid': `Toujours isVerified est false quand on crée un utilisateur.`,
+        'boolean.default': `Par defaut isVerified est false`
+    })
+});
