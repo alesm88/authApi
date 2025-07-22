285fae2223cd65bf2f50a10371442725570361ed
Alejandro <alesm91@hotmail.com>
2025-07-22 14:58:56 +0200

    ajouter des validations Joi sur userValidation.js

--- validations/userValidation.js
+++ validations/userValidation.js
@@ -1,7 +1,16 @@
 import Joi from 'joi';
 
 export const createUserSchema = Joi.object({
-  name: Joi.string()
+    name: Joi.string()
+    .min(2)
+    .max(50)
+    .required()
+    .trim()
+    .messages({
+      'string.empty': `Le prenom est requis.`,
+      'string.min': `Le prenom doit contenir au moins 2 caractères.`,
+    }),
+    lastname: Joi.string()
     .min(2)
     .max(50)
     .required()
@@ -10,18 +19,17 @@ export const createUserSchema = Joi.object({
       'string.empty': `Le nom est requis.`,
       'string.min': `Le nom doit contenir au moins 2 caractères.`,
     }),
-  email: Joi.string()
+    email: Joi.string()
     .email({ tlds: { allow: false } })
     .required()
-    .trim()
+    .lowercase()
     .messages({
       'string.email': `Le format de l'email est invalide.`,
       'string.empty': `L'email est requis.`,
     }),
-  password: Joi.string()
+    password: Joi.string()
     .min(6)
     .required()
-    .trim()
     .messages({
       'string.min': `Le mot de passe doit contenir au moins 6 caractères.`,
       'string.empty': `Le mot de passe est requis.`,
@@ -32,5 +40,10 @@ export const createUserSchema = Joi.object({
     .messages({
         'boolean.valid': `Toujours isVerified est false quand on crée un utilisateur.`,
         'boolean.default': `Par defaut isVerified est false`
+    }),
+    avatar: Joi.string()
+    .min(2)
+    .messages({
+        "string.min": `L'avatar doit conenir au moins 2 caractères.`
     })
 });
