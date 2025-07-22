19d4675267db9590ac8ec1f6ec71f492c5ca5418
Alejandro <alesm91@hotmail.com>
2025-07-22 14:45:15 +0200

    petit modif, pas besoin de stocker les values dans validate ni l'appeler dans le controlleur

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -5,7 +5,5 @@ export const validate = (schema) => (req, res, next) => {
     return res.status(400).json({ error: error.details[0].message });
   }
 
-  // On stocke les données validées dans req.validatedBody
-  req.validatedBody = value;
   next();
 };
